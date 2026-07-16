cbe0ab5fa67d8b905bbdd670f30c8443d3b1bd43
user44 <gaesarang@gmail.com>
2026-07-16 10:06:14 +0900

    fix: 최신 API 키 반영

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const SUPABASE_URL = 'https://kdfxydjvexrndqgnmdqz.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_TFXKV2y0oFermMLgv49fdA_cuSrT6TJ';
+const SUPABASE_KEY = 'sb_publishable_WzRLQbNhDia1JXrIgmUOOg_jHwAls5R';
